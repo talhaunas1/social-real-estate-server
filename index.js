8404edafe198a03b0fb3b8c1f029360e04daa625
talhaunas1 <mtalha4600@gmail.com>
2023-01-18 20:54:16 +0500

    make it better

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import postRoutes from "./src/routes/Posts.js";
+// import authRoutes from "./src/routes/Auth.js";
+// connection to db
+import { connect } from "./src/confiq/db.js";
+
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+const port = 5000;
+
+app.use(express.json());
+
+// app.use('/api/auth', authRoutes)
+app.use("/api/posts", postRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+  connect();
+});
